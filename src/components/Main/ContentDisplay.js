27ffede8cb0eee8bbfe72d904baad11dd3ac1f2b
jrejoire <jrejoire@hotmail.fr>
2020-06-15 11:53:36 +0200

    minor fix ul

--- src/components/Main/ContentDisplay.js
+++ src/components/Main/ContentDisplay.js
@@ -78,7 +78,7 @@ function ContentDisplay({ type, newData, trendyData, activeCategory, activeTrend
           <h3 className="new">New</h3>
         </button>
       </div>
-      <ul role="toolbar" className="categories">
+      <ul className="categories">
         {listedCategories}
       </ul>
       <div className="panels">
