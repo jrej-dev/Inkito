d950344f5fd58c283c51e4dbdfd2563c263b2101
jrejoire <jrejoire@hotmail.fr>
2020-07-28 18:43:25 +0200

    csp config options

--- config-overrides.js
+++ config-overrides.js
@@ -6,18 +6,32 @@ const cspConfigPolicy = {
     'connect-src': ["'self'", "https://anyx.io/", "https://picsum.photos/", "https://inkito-ipfs.herokuapp.com/", "https://hivesigner.com/", "blacklist.usehive.com"],
     'base-uri': "'self'",
     'object-src': "'none'",
-    'script-src': ["'self'","http://www.xiti.com/", "https://logv2.xiti.com/", "nounce-reactDevTool"],
-    'style-src': ["'self'", "'unsafe-inline'", "fonts.googleapis.com", "https://logv2.xiti.com/"],
-    'img-src': ["*", "data:", "blob:", "https://logv2.xiti.com/"],
+    'script-src': ["'self'", "http://www.xiti.com/", "https://logv2.xiti.com/", "nounce-reactDevTool"],
+    'style-src': ["'self'", "'unsafe-inline'", "fonts.googleapis.com"],
+    'img-src': ["*", "data:", "blob:"],
     'media-src': "*",
     'font-src': ["'self'", "data:", "fonts.gstatic.com"],
-    'worker-src': ["'self'","https://storage.googleapis.com"],
+    'worker-src': ["'self'", "https://storage.googleapis.com"],
     'frame-src': ["'self'", "platform.twitter.com", "syndication.twitter.com", "www.youtube.com", "player.vimeo.com", "open.spotify.com", "3speak.online", "emb.d.tube", "player.twitch.tv", "clips.twitch.tv", "www.dailymotion.com", "lbry.tv", "w.soundcloud.com", "www.vimm.tv", "simpleswap.io", "titanembeds.com"]
+};
+
+const additionalOpts = {
+    enabled: true,
+    hashingMethod: 'sha256',
+    hashEnabled: {
+        'script-src': true,
+        'style-src': false
+    },
+    nonceEnabled: {
+        'script-src': true,
+        'style-src': false
+    },
+    processFn: defaultProcessFn
 };
 
 function addCspHtmlWebpackPlugin(config) {
     if (process.env.NODE_ENV === 'production') {
-        config.plugins.push(new cspHtmlWebpackPlugin(cspConfigPolicy));
+        config.plugins.push(new cspHtmlWebpackPlugin(cspConfigPolicy,additionalOpts));
     }
 
     return config;
