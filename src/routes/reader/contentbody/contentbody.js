976d7e8012a67bb50027df222cd50f1e5bd81005
Gcottin <g.cottin@moderlab.com>
2021-02-02 22:44:38 +0100

    image regex

--- src/routes/reader/contentbody/contentbody.js
+++ src/routes/reader/contentbody/contentbody.js
@@ -6,7 +6,8 @@ const md = new Remarkable({html: true}).use(imgify);
 
 const ContentBody = ({ content, description }) => {
   if (content && content.body) {
-    let body = content.body.replace(/(^--.*-$|^\*\*.*\*$|^__.*_$|<hr\/>)/m, '').replace(/<center>\[!\[inkito-banner.png\].*\n*<\/center>$/gm, '').trim();
+    let imgRegex = /<p>(https?:\/\/.*\.(?:png|jpg))<\/p>/g;
+    let body = content.body.replace(/(^--.*-$|^\*\*.*\*$|^__.*_$|<hr\/>)/m, '').replace(/<center>\[!\[inkito-banner.png\].*\n*<\/center>$/gm, '').replace(imgRegex, "<img src='$1' alt='blog-page'/>").trim();
     return (
      <div className={description ? "content align-left" : "content"} dangerouslySetInnerHTML={{ __html: md.render(body) }} />
     )
