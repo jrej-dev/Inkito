ed6c46e186b3decf00b83c15cb13f01a9354dc10
Gcottin <g.cottin@moderlab.com>
2021-03-21 12:37:56 +0100

    fixing bug

--- src/utilities/imgify.js
+++ src/utilities/imgify.js
@@ -6,7 +6,7 @@ function parseTokens(state) {
   var afterRule = false;
   var firstImage = true;
   for (j = 0, l = blockTokens.length; j < l; j++) {
-    if (blockTokens[j].type !== 'inline') { 
+    if (blockTokens[j].type !== 'inline') {
       if (blockTokens[j].type === 'hr') {
         afterRule = true;
       }
@@ -18,35 +18,36 @@ function parseTokens(state) {
     // We scan from the end, to keep position when new tags added.
     // Use reversed logic in links start/end match
     for (i = tokens.length - 1; i >= 0; i--) {
-      let regex = /(https?:\/\/.*\.(?:png|jpg))/;
-      
-      if (tokens[i].type === 'text' && tokens[i].content.match(regex)) {
+      if (tokens[i]) {
+        let regex = /(https?:\/\/.*\.(?:png|jpg))/;
+        if (tokens[i].type === 'text' && tokens[i].content.match(regex)) {
 
-        sources = tokens[i].content.match(regex);
+          sources = tokens[i].content.match(regex);
 
-        alt = sources[0].slice(-14, sources[0].length).replace(/-/gi, "");
+          alt = sources[0].slice(-14, sources[0].length).replace(/-/gi, "");
 
-        nodes.push({
-          type: "image",
-          src: sources[0],
-          title: "",
-          alt: afterRule ? "infoImage" : alt,
-          level: tokens[i].level
-        });
-      }
+          nodes.push({
+            type: "image",
+            src: sources[0],
+            title: "",
+            alt: afterRule ? "infoImage" : alt,
+            level: tokens[i].level
+          });
+        }
 
-      if (!afterRule && tokens[i].type === 'htmltag' && tokens[i].content === "<hr/>") {
-        afterRule = true;
-      }
+        if (!afterRule && tokens[i].type === 'htmltag' && tokens[i].content === "<hr/>") {
+          afterRule = true;
+        }
 
-      if (tokens[i].type === 'image' && firstImage) {
-        firstImage = false;
-      } else if (tokens[i].type === 'image' && afterRule && !firstImage){
-        tokens[i].alt = "infoImage"
-      }
+        if (tokens[i].type === 'image' && firstImage) {
+          firstImage = false;
+        } else if (tokens[i].type === 'image' && afterRule && !firstImage) {
+          tokens[i].alt = "infoImage"
+        }
 
-      if (nodes.length > 0) {
-        blockTokens[j].children = tokens = [].concat(nodes);
+        if (nodes.length > 0) {
+          blockTokens[j].children = tokens = [].concat(nodes);
+        }
       }
     }
   }
